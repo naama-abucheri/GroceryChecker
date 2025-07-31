d5b889f6a88f929fd1760ff7b2d944ad8ab550f6
naama.abucheri <naamaabucheri@gmail.com>
2025-07-31 07:03:55 +0300

    Fix route import issue

--- server/routes/itemRoutes.js
+++ server/routes/itemRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Item = require("../models/items");
+const Item = require("../models/Items");
 
 // GET all items
 router.get("/", async (req, res) => {
